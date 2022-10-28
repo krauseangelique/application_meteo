1b5a4e0ac513ba73dc32e6d37dde0d37f36b8d4d
angelique KRAUSE <krause_angelique@hotmail.com>
2022-10-28 11:20:42 +0200

    change API photos

--- script.js
+++ script.js
@@ -3,3 +3,7 @@
 console.log("Hello Word");
 // weather[0]icon VA DONNER 
 // main.temp VA DONNER 10.56
+// Premier contact avec l'API
+fetch('https://jsonplaceholder.typicode.com/todos/1'), {method: "GET"}
+  .then(response => response.json())
+  .then(json => console.log(json))
