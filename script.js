fb6540d5dc7c949c3d2ca3517e68fdbaf5a23a3c
angelique KRAUSE <krause_angelique@hotmail.com>
2022-11-02 12:13:43 +0100

    mise en place HTML structure balise <template>

--- script.js
+++ script.js
@@ -21,11 +21,11 @@ const lang = "fr";
 const counter = 7;
 
  /* Connexion avec l'api via l'url suivante : 
- api.openweathermap.org/data/2.5/forecast?units=metric&lat=50.6333&lon=5.56667&appid=6f0d59dfcb080cd8495827d107606a39&lang=fr&cnt=7
+ https://api.openweathermap.org/data/2.5/forecast?units=metric&lat=50.6333&lon=5.56667&appid=6f0d59dfcb080cd8495827d107606a39&lang=fr&cnt=7
  */
 
  // API Call
- const weatherUrl = `api.openweathermap.org/data/2.5/forecast?units=metric&lat=${latLiege}&lon=${lonLiege}&appid=${apiKey}&lang=${lang}&cnt=${counter}`;
+ const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${latLiege}&lon=${lonLiege}&appid=${apiKey}&lang=${lang}&cnt=${counter}`;
 console.log(weatherUrl);
 // fetch
 fetch(weatherUrl)
@@ -33,3 +33,39 @@ fetch(weatherUrl)
   .then((response) => {
     console.log(response);
   });
+
+/*
+JavaScript
+Avec le tableau créé et le template défini, on utilise JavaScript pour insérer des lignes dans le tableau dont chacune est construite à partir du template.
+*/
+// On vérifie si le navigateur prend en charge
+// l'élément HTML template en vérifiant la présence
+// de l'attribut content pour l'élément template.
+if ("content" in document.createElement("template")) {
+
+  // On prépare une ligne pour le tableau
+  var template = document.querySelector("#productrow");
+
+  // On clone la ligne et on l'insère dans le tableau
+  var tbody = document.querySelector("tbody");
+  var clone = document.importNode(template.content, true);
+  var td = clone.querySelectorAll("td");
+  td[0].textContent = "1235646565";
+  td[1].textContent = "Stuff";
+
+  tbody.appendChild(clone);
+
+  // On fait de même pour une autre ligne
+  var clone2 = document.importNode(template.content, true);
+  td = clone2.querySelectorAll("td");
+  td[0].textContent = "0384928528";
+  td[1].textContent = "Acme Kidney Beans";
+
+  // Puis on insère
+  tbody.appendChild(clone2);
+
+} else {
+  // Une autre méthode pour ajouter les lignes
+  // car l'élément HTML n'est pas pris en charge.
+  console.error('le navigateur ne prend pas en charge l\'élément HTML template');
+}
